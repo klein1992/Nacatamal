cff0c18b5710560b96954c35c7333930fe0ae644
Eduardo Wheelock <=>
2021-03-26 22:39:16 -0400

    Add cart items array

---
+++ cart_items.js
@@ -0,0 +1,17 @@
+export const CART_ITEMS = [
+    {
+        imageName: '/chile_relleno.jpg', imageTitle: 'Chile Relleno',
+        title: "Chile rellenito", description: 'El Mejor Chile Relleno que usted ha probado o se le devuelve su dinero!',
+        buttonText: 'Compre Ya', buttonText2: 'Ingredientes'
+    },
+    {
+        imageName: '/nacatamal_natural.jpg', imageTitle: 'Nacatamal Natural',
+        title: "Nacatamal Grande", description: 'El Mejor Nacatamal que usted ha probado o se le da un chile relleno gratis!',
+        buttonText: 'Compre Ya', buttonText2: 'Ingredientes'
+    },
+    {
+        imageName: '/arroz.jpg', imageTitle: 'Arroz a la Valenciana',
+        title: "Arroz a la Valenciana", description: 'El Mejor Arroz a la Valenciana que usted ha probado o se le da un nacatamal gratis! (chiquito)',
+        buttonText: 'Compre Ya', buttonText2: 'Ingredientes'
+    },
+];
